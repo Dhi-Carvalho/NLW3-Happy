f6835c3e0942a8cab55721d5a0a047a6aaeeca6b
Dhi-Carvalho <dhi.carvalho85@gmail.com>
2021-03-16 19:34:27 -0300

    Structuring the server and its routes

---
+++ src/database/fakedata.js
@@ -0,0 +1,49 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-27.222633",
+        lng: "-49.6455874",
+        name: "Lar das Meninas",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1537655780520-1e392ead81f2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+
+            "https://images.unsplash.com/photo-1526634332515-d56c5fd16991?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+
+            "https://images.unsplash.com/photo-1502086223501-7ea6ecd79368?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1323&q=80",
+
+            "https://images.unsplash.com/photo-1589424987100-72303ec43d04?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=638&q=80",
+
+            "https://images.unsplash.com/photo-1598984533755-aa0aa797cfd5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=674&q=80",
+
+            "https://images.unsplash.com/photo-1534216801749-bb1db8970e54?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+        ],
+        instructions: "Venha quando se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 8h até 18h.",
+        opening_on_weekends: "1"
+    },
+
+    {
+        id: 2,
+        lat: "-27.000633",
+        lng: "-45.6455874",
+        name: "Lar das Meninas",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1537655780520-1e392ead81f2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+
+            "https://images.unsplash.com/photo-1526634332515-d56c5fd16991?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+
+            "https://images.unsplash.com/photo-1502086223501-7ea6ecd79368?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1323&q=80",
+
+            "https://images.unsplash.com/photo-1589424987100-72303ec43d04?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=638&q=80",
+
+            "https://images.unsplash.com/photo-1598984533755-aa0aa797cfd5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=674&q=80",
+
+            "https://images.unsplash.com/photo-1534216801749-bb1db8970e54?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+        ],
+        instructions: "Venha quando se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 8h até 18h.",
+        opening_on_weekends: "0"
+    },
+]
