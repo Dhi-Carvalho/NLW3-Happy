c15c1ae4487c542b6edd8c96b15f3f1fcbf7ea26
Dhi-Carvalho <dhi.carvalho85@gmail.com>
2021-03-18 07:11:41 -0300

    Formatting the code using Prettier

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -3,7 +3,7 @@ const map = L.map("mapid").setView([-27.222633, -49.6455874], 15);
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 const icon = L.icon({
-  iconUrl: "./public/images/map-marker.svg",
+  iconUrl: "/images/map-marker.svg",
   iconSize: [58, 68],
   iconAnchor: [29, 68],
 });
@@ -11,53 +11,54 @@ const icon = L.icon({
 let marker;
 
 map.on("click", (event) => {
-    const lat = event.latlng.lat;
-    const lng = event.latlng.lng;
+  const lat = event.latlng.lat;
+  const lng = event.latlng.lng;
 
-    document.querySelector('[name=lat]').value = lat;
-    document.querySelector('[name=lng]').value = lng;
+  document.querySelector("[name=lat]").value = lat;
+  document.querySelector("[name=lng]").value = lng;
 
-    marker && map.removeLayer(marker)
+  marker && map.removeLayer(marker);
 
-    marker = L.marker([lat, lng], {icon})
-    .addTo(map)
-})
+  marker = L.marker([lat, lng], { icon }).addTo(map);
+});
 
 function addPhotoField() {
-    const container = document.querySelector('#images')
-    const fieldsContainer = document.querySelectorAll('.new-upload')
-    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true)
-    const input = newFieldContainer.children[0]
-    
-    if(input.value == "") {
-        return
-    }
-    
-    input.value=""
+  const container = document.querySelector("#images");
+  const fieldsContainer = document.querySelectorAll(".new-upload");
+  const newFieldContainer = fieldsContainer[
+    fieldsContainer.length - 1
+  ].cloneNode(true);
+  const input = newFieldContainer.children[0];
 
-    container.appendChild(newFieldContainer)
+  if (input.value == "") {
+    return;
+  }
+
+  input.value = "";
+
+  container.appendChild(newFieldContainer);
 }
 
-function deleteField(event)  {
-    const span = event.currentTarget
-    const fieldsContainer = document.querySelectorAll('.new-upload')
+function deleteField(event) {
+  const span = event.currentTarget;
+  const fieldsContainer = document.querySelectorAll(".new-upload");
 
-    if(fieldsContainer.length <= 1) {
-        span.parentNode.children[0].value=""
-        return
-    }
+  if (fieldsContainer.length <= 1) {
+    span.parentNode.children[0].value = "";
+    return;
+  }
 
-    span.parentNode.remove();
+  span.parentNode.remove();
 }
 
 function toggleSelected(event) {
-    document.querySelectorAll('.button-select button')
-    .forEach(button => button.classList.remove('active'))
+  document
+    .querySelectorAll(".button-select button")
+    .forEach((button) => button.classList.remove("active"));
 
-    const button = event.currentTarget
-    button.classList.add('active')
+  const button = event.currentTarget;
+  button.classList.add("active");
 
-    const input = document.querySelector('[name="open_on_weekends"]')
-    input.value = button.dataset.value
+  const input = document.querySelector('[name="open_on_weekends"]');
+  input.value = button.dataset.value;
 }
-
