96aac267c7ce7ae36b32d90f1d4e826bf28c4c65
Dhi-Carvalho <dhi.carvalho85@gmail.com>
2021-03-18 07:13:02 -0300

    Update of the mark on the map

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -1,12 +1,15 @@
 const options = {
-    dragging: false,
-    touchZoom: false,
-    doubleClickZoom: false,
-    scrollWhellZoom: false,
-    zoomControl: false
-}
+  dragging: false,
+  touchZoom: false,
+  doubleClickZoom: false,
+  scrollWhellZoom: false,
+  zoomControl: false,
+};
 
-const map = L.map("mapid", options).setView([-27.222633, -49.6455874], 15);
+const lat = document.querySelector("span[data-lat]").dataset.lat;
+const lng = document.querySelector("span[data-lng]").dataset.lng;
+
+const map = L.map("mapid", options).setView([lat, lng], 15);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
@@ -17,20 +20,20 @@ const icon = L.icon({
   popupAnchor: [170, 2],
 });
 
-L.marker([-27.222633, -49.6455874], { icon }).addTo(map)
+L.marker([lat, lng], { icon }).addTo(map);
 
 function selectImage(event) {
-  const button = event.currentTarget
-  const buttons = document.querySelectorAll(".images button")
-  buttons.forEach(removeActiveClass)
+  const button = event.currentTarget;
+  const buttons = document.querySelectorAll(".images button");
+  buttons.forEach(removeActiveClass);
 
   function removeActiveClass(button) {
-    button.classList.remove("active")
+    button.classList.remove("active");
   }
 
-  const image = button.children[0]
-  const imageContainer = document.querySelector(".orphanage-details > img")
-  imageContainer.src = image.src
+  const image = button.children[0];
+  const imageContainer = document.querySelector(".orphanage-details > img");
+  imageContainer.src = image.src;
 
-  button.classList.add("active")
+  button.classList.add("active");
 }
